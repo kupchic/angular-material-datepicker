e27a31a5a59c2f605b16024a7808fa3c24e48b13
akupchenia <kup4enya1999@gmail.com>
2026-04-01 21:26:02 +0300

    Remove Angular animations from datetime-picker and color-picker; replace with CSS animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideNativeDateAdapter } from '@angular/material/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideNativeDateAdapter(),
-    provideAnimations(),
     provideZoneChangeDetection(),
     provideRouter(APP_ROUTES),
   ],
